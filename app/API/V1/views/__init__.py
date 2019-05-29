a38209945adfdfcd2d1fb73e83b4d28265ccdf14
Sheikhawadh19 <shekytaylor@gmail.com>
2019-05-29 11:47:29 +0300

    further edits

--- app/API/V1/views/__init__.py
+++ app/API/V1/views/__init__.py
@@ -3,4 +3,4 @@ from flask import Blueprint
 # This instance of a Blueprint that represents the authentication blueprint
 auth_blueprint = Blueprint('auth', __name__)
 
-from . import views
+from app.API.V1.views import views
--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
+from flask import request, jsonify, abort, make_response
 
 # local import
 from instance.config import app_config
@@ -10,7 +11,7 @@ db = SQLAlchemy()
 from flask import request, jsonify, abort
 
 def create_app(config_name):
-    from app.API.V1.models.models import Ireporter
+    from app.API.V1.models.models import Ireporter, User
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
@@ -20,84 +21,123 @@ def create_app(config_name):
    
     @app.route('/API/V1/Ireporter/', methods=['POST', 'GET'])
     def ireporters():
-        if request.method == "POST":
-            name = str(request.data.get('name', ''))
-            if name:
-                ireporter = Ireporter(name=name)
-                ireporter.save()
-                response = jsonify({
-                    'id': ireporter.id,
-                    'name': ireporter.name,
-                    'email': ireporter.email,
-                    'comment': ireporter.comment,
-                    'date_created': ireporter.date_created,
-                    'date_modified': ireporter.date_modified
-                })
-                response.status_code = 201
-                return response
-        else:
-            # GET
-            ireporters = Ireporter.get_all()
-            results = []
+        # Get the access token from the header
+        auth_header = request.headers.get('Authorization')
+        access_token = auth_header.split(" ")[1]
 
-            for ireporter in ireporters:
-                obj = {
-                    'id': ireporter.id,
-                    'name': ireporter.name,
-                    'email': ireporter.email,
-                    'comment': ireporter.comment,
-                    'date_created': ireporter.date_created,
-                    'date_modified': ireporter.date_modified
+        if access_token:
+         # Attempt to decode the token and get the User ID
+            user_id = User.decode_token(access_token)
+            if not isinstance(user_id, str):
+                # Go ahead and handle the request, the user is authenticated
+
+                if request.method == "POST":
+                    name = str(request.data.get('name', ''))
+                    if name:
+                        ireporter = Ireporter(name=name, created_by=user_id)
+                        ireporter.save()
+                        response = jsonify({
+                            'id': ireporter.id,
+                            'name': ireporter.name,
+                            'date_created': ireporter.date_created,
+                            'date_modified': ireporter.date_modified,
+                            'created_by': user_id
+                        })
+
+                        return make_response(response), 201
+
+                else:
+                    # GET all the bucketlists created by this user
+                    ireporters = Ireporter.query.filter_by(created_by=user_id)
+                    results = []
+
+                    for Ireporter in ireporters:
+                        obj = {
+                            'id': ireporter.id,
+                            'name': ireporter.name,
+                            'date_created': ireporter.date_created,
+                            'date_modified': ireporter.date_modified,
+                            'created_by': ireporter.created_by
+                        }
+                        results.append(obj)
+
+                    return make_response(jsonify(results)), 200
+            else:
+                # user is not legit, so the payload is an error message
+                message = user_id
+                response = {
+                    'message': message
                 }
-                results.append(obj)
-            response = jsonify(results)
-            response.status_code = 200
-            return response
+                return make_response(jsonify(response)), 401
     @app.route('/API/V1/Ireporter/<int:id>', methods=['GET', 'PUT', 'DELETE'])
     def ireporter_manipulation(id, **kwargs):
-     # retrieve a buckelist using it's ID
-        ireporter = Ireporter.query.filter_by(id=id).first()
-        if not ireporter:
-            # Raise an HTTPException with a 404 not found status code
-            abort(404)
+        # get the access token from the authorization header
+        auth_header = request.headers.get('Authorization')
+        access_token = auth_header.split(" ")[1]
 
-        if request.method == 'DELETE':
-            ireporter.delete()
-            return {
-            "message": "ireporter {} deleted successfully".format(ireporter.id) 
-         }, 200
+        if access_token:
+            # Get the user id related to this access token
+            user_id = User.decode_token(access_token)
 
-        elif request.method == 'PUT':
-            name = str(request.data.get('name', ''))
-            ireporter.name = name
-            ireporter.save()
-            response = jsonify({
-              'id': ireporter.id,
-              'name': ireporter.name,
-              'email': ireporter.email,
-              'comment': ireporter.comment,
-              'date_created': ireporter.date_created,
-              'date_modified': ireporter.date_modified
-            })
-            response.status_code = 200
-            return response
-        else:
-            # GET
-            response = jsonify({
-                'id': ireporter.id,
-                'name': ireporter.name,
-                'email': ireporter.email,
-                'comment': ireporter.comment,
-                'date_created': ireporter.date_created,
-                'date_modified': ireporter.date_modified
-            })
-            response.status_code = 200
-            return response
-            
-    from .API.V1.views import auth_blueprint
-    app.register_blueprint(auth_blueprint)
+            if not isinstance(user_id, str):
+                # If the id is not a string(error), we have a user id
+                # Get the bucketlist with the id specified from the URL (<int:id>)
+                ireporter = Ireporter.query.filter_by(id=id).first()
+                if not ireporter:
+                    # There is no bucketlist with this ID for this User, so
+                    # Raise an HTTPException with a 404 not found status code
+                    abort(404)
+
+                if request.method == "DELETE":
+                    # delete the bucketlist using our delete method
+                    ireporter.delete()
+                    return {
+                        "message": "ireporter {} deleted".format(ireporter.id)
+                    }, 200
+
+                elif request.method == 'PUT':
+                    # Obtain the new name from the request data
+                    name = str(request.data.get('name', ''))
+
+                    ireporter.name = name
+                    ireporter.save()
+
+                    response = {
+                        'id': ireporter.id,
+                        'name': ireporter.name,
+                        'date_created': ireporter.date_created,
+                        'date_modified': ireporter.date_modified,
+                        'created_by': ireporter.created_by
+                    }
+                    return make_response(jsonify(response)), 200
+                else:
+                    # Handle GET request, sending back the bucketlist to the user
+                    response = {
+                        'id': ireporter.id,
+                        'name': ireporter.name,
+                        'date_created': ireporter.date_created,
+                        'date_modified': ireporter.date_modified,
+                        'created_by': ireporter.created_by
+                    }
+                    return make_response(jsonify(response)), 200
+            else:
+                # user is not legit, so the payload is an error message
+                message = user_id
+                response = {
+                    'message': message
+                }
+                # return an error response, telling the user he is Unauthorized
+                return make_response(jsonify(response)), 401
+
+   
    
 
+    # import the authentication blueprint and register it on the app
+    from .API.V1.views import auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
     return app
+
+
 
 
